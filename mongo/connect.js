4b2368b5ad676d6c068dfcb639201a96b760c57e
forbesashli <44144720+forbesashli@users.noreply.github.com>
2021-01-09 23:54:42 -0800

    create country time

--- mongo/connect.js
+++ mongo/connect.js
@@ -3,41 +3,75 @@ const extMain = require ('./main')
 var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
-
+require('dotenv').config();
 const { MongoClient } = require("mongodb");
 
 var collection;
+
+var ListTest = {
+    
+        name: "Lovely Loft",
+        summary: "A charming loft in Paris",
+        bedrooms: 1,
+        bathrooms: 1
+    
+}
  
 // Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://testuser12:nwhacks12@cluster0.n1vnq.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(url);
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 async function run() {
     try {
         await client.connect();
-        await extMain.listDatabases(client);
-      //  await createListing(createListing,);
         console.log("Connected correctly to server");
+        await mainFcns();
     } catch (err) {
         console.log(err.stack);
     }
     finally {
        await client.close();
+       console.log("Disconnected");
     }
 }
 run().catch(console.dir);
 
+async function mainFcns (){
+   // await extMain.listDatabases(client);
+   // await extMain.createListing(client,ListTest);
 
-function myFunction(){
-    app.use("/public", express.static(__dirname + "/public")); 
+  /**   await extMain.createMultipleListings(client, [
+        {
+            name: "Infinite Views",
+            summary: "Modern home with infinite views from the infinity pool",
+            property_type: "House",
+            bedrooms: 5,
+            bathrooms: 4.5,
+            beds: 5
+        },
+        {
+            name: "Private room in London",
+            property_type: "Apartment",
+            bedrooms: 1,
+            bathroom: 1
+        },
+        {
+            name: "Beautiful Beach House",
+            summary: "Enjoy relaxed beach living in this house with a private beach",
+            bedrooms: 4,
+            bathrooms: 2.5,
+            beds: 7,
+            last_review: new Date()
+        }
+    ]);*/
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+    //await extMain.findOneListingByName(client, "Infinite Views");
+    /** 
+    await extMain.findMinListings(client, {
+        minimumNumberOfBedrooms: 4,
+        minimumNumberOfBathrooms: 2,
+        maximumNumberOfResults: 5
+    });*/
 
-http.listen(3000, '0.0.0.0', function(){
-    console.log('listening on *:3000');
-});
-    
-output = {};
-collection.insert(output, function (err, result) {});
+    await extMain.updateListingByName(client, "Infinite Views", { score: 0.1});
+
 }
--- mongo/main.js
+++ mongo/main.js
@@ -11,4 +11,79 @@ async function createListing(client, newListing){
     console.log(`New listing created with the following id: ${result.insertedId}`);
 }
 
-module.exports={listDatabases,createListing};
+async function createCountry(client, newCountry){
+    const result = await client.db("JSON_Objects").collection("nwHack").insertOne(newCountry);
+}
+
+async function createMultipleListings(client, newListings){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+    console.log(result.insertedIds);
+}
+
+async function findOneListingByName(client, nameOfListing) {
+    result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .findOne({ name: nameOfListing });
+
+    if (result) {
+        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+        console.log(result);
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
+
+async function findMinListings(client, {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+} = {}) {
+    const cursor = client.db("sample_airbnb").collection("listingsAndReviews")
+        .find({
+            bedrooms: { $gte: minimumNumberOfBedrooms },
+            bathrooms: { $gte: minimumNumberOfBathrooms }
+        }
+        )
+        .sort({ last_review: -1 })
+        .limit(maximumNumberOfResults);
+
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+        console.log(`Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms:`);
+        results.forEach((result, i) => {
+            date = new Date(result.last_review).toDateString();
+
+            console.log();
+            console.log(`${i + 1}. name: ${result.name}`);
+            console.log(`   _id: ${result._id}`);
+            console.log(`   bedrooms: ${result.bedrooms}`);
+            console.log(`   bathrooms: ${result.bathrooms}`);
+            console.log(`   most recent review date: ${new Date(result.last_review).toDateString()}`);
+        });
+    } else {
+        console.log(`No listings found with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`);
+    }
+}
+
+async function updateListingByName(client, nameOfListing, updatedListing) {
+    result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateOne({ name: nameOfListing }, { $set: updatedListing });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+// Actual data monkey time
+
+async function updateCountryScorebyName(client, currentCountry, updatedData) {
+    result = await client.db("JSON_Objects").collection("nwHack").updateOne({ name: currentCountry }, { $set: updatedData });
+}
+
+async function createCountry(client, newCountry){
+    const result = await client.db("JSON_Objects").collection("nwHack").insertOne(newCountry);
+}
+
+
+module.exports = {listDatabases,createListing,createMultipleListings,findOneListingByName,findMinListings,updateListingByName,createCountry,updateCountryScorebyName};
