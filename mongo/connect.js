0d66ce16f9e4b734177131784dab0732b04dd67e
forbesashli <44144720+forbesashli@users.noreply.github.com>
2021-01-09 22:30:25 -0800

    client update

--- mongo/connect.js
+++ mongo/connect.js
@@ -1,4 +1,12 @@
+const extMain = require ('./main')
+
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+
 const { MongoClient } = require("mongodb");
+
+var collection;
  
 // Replace the following with your Atlas connection string                                                                                                                                        
 const url = "mongodb+srv://testuser12:nwhacks12@cluster0.n1vnq.mongodb.net/<dbname>?retryWrites=true&w=majority";
@@ -6,12 +14,30 @@ const client = new MongoClient(url);
 async function run() {
     try {
         await client.connect();
+        await extMain.listDatabases(client);
+      //  await createListing(createListing,);
         console.log("Connected correctly to server");
     } catch (err) {
         console.log(err.stack);
     }
     finally {
-        await client.close();
+       await client.close();
     }
 }
 run().catch(console.dir);
+
+
+function myFunction(){
+    app.use("/public", express.static(__dirname + "/public")); 
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, '0.0.0.0', function(){
+    console.log('listening on *:3000');
+});
+    
+output = {};
+collection.insert(output, function (err, result) {});
+}
---
+++ mongo/main.js
@@ -0,0 +1,14 @@
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function createListing(client, newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+module.exports={listDatabases,createListing};
