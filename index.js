874ec8df3cc82a5084168f0d0125fdb92a8e47fd
Mikhaela Layon <mapl.layon@gmail.com>
2021-01-10 11:30:08 -0800

    fixed weird express.js issue

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// var http = require('http'),
-//     fs = require('fs');
-
-
-// fs.readFile('./index.html', function (err, html) {
-//     if (err) {
-//         throw err; 
-//     }       
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(process.env.PORT || 5000)
-// });
-
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -25,13 +10,6 @@ app.use(express.static('./'));
 app.use(express.static(__dirname + '/pages'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/incoming', function(req, res) {
-    fs.readFile('../data/incoming.json', (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
-    });
 });
 
 app.listen(port, () => {
